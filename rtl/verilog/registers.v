8c5472687b58397daaf100a3769ba3c1d044b9ad
Patrick Hoey <patrickrhoey@gmail.com>
2022-04-16 21:55:58 -0400

    updated notes and registers

--- rtl/verilog/registers.v
+++ rtl/verilog/registers.v
@@ -11,16 +11,33 @@
 //-----------------------------------------------------------------------------
 // Module Declaration
 //-----------------------------------------------------------------------------
-module registers (/*AUTOARG*/) ;
+module registers (/*AUTOARG*/
+   // Outputs
+   rd_data_1_w_o, rd_data_2_w_o,
+   // Inputs
+   clk_w_i, res_w_i_h, rd_reg_1_w_i, rd_reg_2_w_i, wr_reg_w_i, wr_data_w_i,
+   reg_wr_flag_w_i
+   ) ;
 
 
    //-----------------------------------------------------------------------------
    // Inputs
    //-----------------------------------------------------------------------------
+   input wire clk_w_i;
+   input wire res_w_i_h;
+
+   input wire [4:0] rd_reg_1_w_i;
+   input wire [4:0] rd_reg_2_w_i;
+   input wire [4:0] wr_reg_w_i;
+   input wire [31:0] wr_data_w_i;
+
+   input wire        reg_wr_flag_w_i;
 
    //-----------------------------------------------------------------------------
    // Outputs
    //-----------------------------------------------------------------------------
+   output wire [31:0] rd_data_1_w_o;
+   output wire [31:0] rd_data_2_w_o;
 
    //-----------------------------------------------------------------------------
    // Parameters
@@ -29,6 +46,7 @@ module registers (/*AUTOARG*/) ;
    //-----------------------------------------------------------------------------
    // Internal Registers and Wires
    //-----------------------------------------------------------------------------
+   reg [31:0]         register [31:0];
 
    //-----------------------------------------------------------------------------
    // Instantiations
@@ -37,10 +55,24 @@ module registers (/*AUTOARG*/) ;
    //-----------------------------------------------------------------------------
    // RTL
    //-----------------------------------------------------------------------------
+   integer            i;
+   always @ (posedge clk_w_i or posedge res_w_i_h) begin
+      if (res_w_i_h) begin
+         for (i = 0; i < 32; i = i + 1) begin
+            register[i] <= 0;
+         end
+      end else begin
+         if (reg_wr_flag_w_i) begin
+            register[wr_reg_w_i] <= wr_data_w_i;
+         end
+      end
+   end
 
    //-----------------------------------------------------------------------------
    // Assigns
    //-----------------------------------------------------------------------------
+   assign rd_data_1_w_o = register[rd_reg_1_w_i];
+   assign rd_data_2_w_o = register[rd_reg_2_w_i];
 
 endmodule // registers
 
