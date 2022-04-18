32316e8b29354cdf02836e831800b975d3c9a2e8
Patrick Hoey <patrickrhoey@gmail.com>
2022-04-18 16:53:35 -0400

    added isa notes and imm gen first attempt written, no tb done

--- rtl/verilog/imm_gen.v
+++ rtl/verilog/imm_gen.v
@@ -17,18 +17,27 @@ module imm_gen (/*AUTOARG*/ ) ;
    //-----------------------------------------------------------------------------
    // Inputs
    //-----------------------------------------------------------------------------
+   input [31:0] instr_w_i;
 
    //-----------------------------------------------------------------------------
    // Outputs
    //-----------------------------------------------------------------------------
+   output [31:0] imm_w_o;
 
    //-----------------------------------------------------------------------------
    // Parameters
    //-----------------------------------------------------------------------------
+   parameter J_TYPE = 7'b1101111, // Concatanate compare field from Op Code
+      U_TYPE = 6'b010111,
+      S_TYPE = 4'b0011,
+      B_TYPE = 3'b011,
+      I_TYPE = 2'b11;           // I Type is default Else Case
 
    //-----------------------------------------------------------------------------
    // Internal Registers and Wires
    //-----------------------------------------------------------------------------
+   reg [6:0]    opcode_r;
+   reg [31:0]   imm_r;
 
    //-----------------------------------------------------------------------------
    // Instantiations
@@ -37,9 +46,28 @@ module imm_gen (/*AUTOARG*/ ) ;
    //-----------------------------------------------------------------------------
    // RTL
    //-----------------------------------------------------------------------------
+   always @ (*) begin
+      opcode_r = instr_w_i[6:0];
+
+      // Comparison order is based on prio, do not modify order without checking prio
+      // Sign extend all results to 32-bits
+      if (opcode_r == J_TYPE) begin
+      end else if ({opcode_r[6], opcode_r[4:0]} == U_TYPE) begin
+         imm_r = {{12{instr_w_i[31]}}, instr_w_i[31:12]};
+      end else if ({opcode_r[6], opcode_r[4], opcode_r[1:0]} == S_TYPE) begin
+         imm_r = {{20{instr_w_i[31]}}, instr_w_i[31:25], instr_w_i[11:7]};
+      end else if ({opcode_r[4], opcode_r[1:0]} == B_TYPE) begin
+         imm_r = {{19{instr_w_i[31]}}, instr_w_i[31], instr_w_i[7], instr_w_i[30:25], instr_w_i[11:8], 1'b0};
+      end else begin // I_TYPE or a don't care
+         imm_r = {{20{instr_w_i[31]}}, instr_w_i[31:25]};
+      end
+
+   end
 
    //-----------------------------------------------------------------------------
    // Assigns
    //-----------------------------------------------------------------------------
+   assign imm_w_o = imm_r;
 
-endmodule // imm_gen
+
+ endmodule // imm_gen
